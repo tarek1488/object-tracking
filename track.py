70ce667657b57934875ca6ffe94e95ade97163ad
Tarek Ashraf <tarekshalaby2015@gmail.com>
2024-08-07 15:57:13 +0300

    model after finetuning integrated in track mode successfully

--- track.py
+++ track.py
@@ -7,8 +7,8 @@ from ultralytics.utils.plotting import Annotator, colors
 
 track_history = defaultdict(lambda: [])
 
-model = YOLO("yolov8n-seg.pt")  # segmentation model
-cap = cv2.VideoCapture("Cars Moving On Road Stock Footage - Free Download.mp4")
+model = YOLO(r"runs\segment\train22\weights\best.pt")  # segmentation model
+cap = cv2.VideoCapture("Car passing by in a Highway - Royalty Free Stock Video - (Copyright Free) Download.mp4")
 w, h, fps = (int(cap.get(x)) for x in (cv2.CAP_PROP_FRAME_WIDTH, cv2.CAP_PROP_FRAME_HEIGHT, cv2.CAP_PROP_FPS))
 
 out = cv2.VideoWriter("instance-segmentation-object-tracking.avi", cv2.VideoWriter_fourcc(*"MJPG"), fps, (w, h))
@@ -33,7 +33,7 @@ while True:
             annotator.seg_bbox(mask=mask, mask_color=color, label=str(track_id), txt_color=txt_color)
 
     out.write(im0)
-    cv2.imshow("instance-segmentation-object-tracking", im0)
+    cv2.imshow("output-before-fine-tuning", im0)
 
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
